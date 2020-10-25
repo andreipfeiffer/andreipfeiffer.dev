266269c4faae1df7dd7b18fd8ddd017d1795e5dc
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-10-25 19:12:09 +0200

    Embed mailchimp signup form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -19,7 +19,11 @@ export default function Contact() {
         <br />
         <label>
           <span>Subject:</span>
-          <input type="text" />
+          <select>
+            <option value="contact">General inquiry</option>
+            <option value="speaking">Speaking engagement</option>
+            <option value="workshop">Workhops or Trainings</option>
+          </select>
         </label>
         <br />
         <label>
@@ -40,11 +44,86 @@ export default function Contact() {
 
 function Subscribe() {
   return (
-    <form action="#" method="post">
-      <label>
-        <span>Email address:</span>
-        <input type="text" />
-      </label>
-    </form>
+    <div id="mc_embed_signup">
+      <form
+        action="https://dev.us2.list-manage.com/subscribe/post?u=a7ac7926ed477a5e810ed46f7&amp;id=51b196576b"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+      >
+        <div id="mc_embed_signup_scroll">
+          <div className="mc-field-group">
+            <label htmlFor="mce-EMAIL">
+              Email Address <span className="asterisk">*</span>
+            </label>
+            <input
+              type="email"
+              defaultValue=""
+              name="EMAIL"
+              className="required email"
+              id="mce-EMAIL"
+            />
+          </div>
+
+          <div className="mc-field-group">
+            <label htmlFor="mce-FNAME">First Name </label>
+            <input
+              type="text"
+              defaultValue=""
+              name="FNAME"
+              className=""
+              id="mce-FNAME"
+            />
+          </div>
+
+          <div className="mc-field-group">
+            <label htmlFor="mce-LNAME">Last Name </label>
+            <input
+              type="text"
+              defaultValue=""
+              name="LNAME"
+              className=""
+              id="mce-LNAME"
+            />
+          </div>
+
+          <div id="mce-responses" className="clear">
+            <div
+              className="response"
+              id="mce-error-response"
+              style={{ display: "none" }}
+            ></div>
+            <div
+              className="response"
+              id="mce-success-response"
+              style={{ display: "none" }}
+            ></div>
+          </div>
+
+          <div
+            style={{ position: "absolute", left: "-5000px" }}
+            aria-hidden="true"
+          >
+            <input
+              type="text"
+              name="b_a7ac7926ed477a5e810ed46f7_51b196576b"
+              tabIndex={-1}
+              defaultValue=""
+            />
+          </div>
+          <div className="clear">
+            <input
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className="button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
