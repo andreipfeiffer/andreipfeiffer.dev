93bd4aeee86eb371913416af7c613e586fa3d9ba
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-11-01 13:44:05 +0200

    Adjust layout width

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "./layout.module.css";
+import styles from "./layout.module.scss";
 import { Link } from "./Link";
 import useDarkMode from "use-dark-mode";
 
