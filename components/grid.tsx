e51a460c9f018f3886c45ceec7126ca7bea01d3e
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-03-29 09:49:24 +0300

    Add custom className to Grid

--- components/grid.tsx
+++ components/grid.tsx
@@ -5,12 +5,13 @@ import styles from "./grid.module.scss";
 type Props = {
   children?: React.ReactNode;
   as?: string;
+  className?: string;
 };
 
-export function Grid({ children, as = "div" }: Props) {
+export function Grid({ children, as = "div", className }: Props) {
   return React.createElement(
     as,
-    { className: classNames(styles.grid, styles.layout) },
+    { className: classNames(styles.grid, styles.layout, className) },
     children
   );
 }
@@ -19,18 +20,22 @@ type ItemProps = Props & {
   span?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 };
 
-function Item({ children, span = 1, as = "div" }: ItemProps) {
+function Item({ children, span = 1, as = "div", className }: ItemProps) {
   return React.createElement(
     as,
-    { className: classNames(styles.item, styles[`size_${span}`]) },
+    { className: classNames(styles.item, styles[`size_${span}`], className) },
     children
   );
 }
 
 // used for single full width element
 // similar with having a Grid + Col span 12
-Grid.Full = ({ children, as = "div" }: Props) => {
-  return React.createElement(as, { className: styles.layout }, children);
+Grid.Full = ({ children, as = "div", className }: Props) => {
+  return React.createElement(
+    as,
+    { className: classNames(styles.layout, className) },
+    children
+  );
 };
 
 Grid.Col = Item;
