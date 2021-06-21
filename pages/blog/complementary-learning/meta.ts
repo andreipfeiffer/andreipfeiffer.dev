bddd8561d3b8f222c16bde2253ffa80008fcd20f
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-06-21 17:26:06 +0300

    Add new article

---
+++ pages/blog/complementary-learning/meta.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "Complementary learning",
+  date: "2021-06-10",
+  tags: ["misc"],
+  tags_extra: ["learning", "asking", "understanding"],
+  intro:
+    "We took some courses, attended a few workshops and read a ton of articles. We have even worked on a couple of projects. What's next?",
+  isPublished: true,
+  isArchived: false,
+};
