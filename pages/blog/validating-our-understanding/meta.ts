530e228a4dda861a8b4bbc6b0fa32a450def288a
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-06-21 22:00:20 +0300

    Add article

---
+++ pages/blog/validating-our-understanding/meta.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "Validating our understanding",
+  date: "2021-06-21",
+  tags: ["misc"],
+  tags_extra: ["knowledge", "measure", "understanding", "analogy"],
+  intro:
+    "There is no absolute measurement tool or technique to determine our level of understanding or a particular concept. However, there are some heuristics that we can use to evaluate ourselves.",
+  isPublished: false,
+  isArchived: false,
+};
