3a36073524d6a23f953ab2bec681448f3c5d643c
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-04 22:10:20 +0300

    Rename article

---
+++ pages/blog/developing-ui-connected-with-web-api/meta.ts
@@ -0,0 +1,13 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "Developing UIs connected to web APIs",
+  date: "2021-07-03",
+  tags: ["ui"],
+  tags_extra: ["api", "development", "remote", "local", "fake"],
+  intro:
+    "As UI developers, we have multiple methods we could use to connect to web APIs during development. We'll explore 3 different approaches, analyzing their strong points and limitations.",
+  isPublished: false,
+  isArchived: false,
+  cover: "/images/share/developing-ui-connected-with-web-api.png",
+};
