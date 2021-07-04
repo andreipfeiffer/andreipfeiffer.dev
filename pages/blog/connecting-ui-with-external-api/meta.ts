096183242d75328d17657b15ffeebd399a941383
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-04 18:38:11 +0300

    Add new article

---
+++ pages/blog/connecting-ui-with-external-api/meta.ts
@@ -0,0 +1,13 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "Developing UIs connected to web APIs",
+  date: "2021-07-03",
+  tags: ["ui"],
+  tags_extra: [],
+  intro:
+    "As UI developers, we have multiple methods we could use to connect to RESTful APIs during development. We'll debate 3 different approaches, each of them providing certain benefits and limitations.",
+  isPublished: false,
+  isArchived: false,
+  // cover: "/images/share/validating-our-understanding.png",
+};
