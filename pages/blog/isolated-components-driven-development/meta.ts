84d720197eaaf1c78d4188d56f1155bca885da9f
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-05 18:10:42 +0300

    Add new article

---
+++ pages/blog/isolated-components-driven-development/meta.ts
@@ -0,0 +1,19 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "Isolated Components Driven Development",
+  date: "2021-07-03",
+  tags: ["ui"],
+  tags_extra: [
+    "decoupling",
+    "components",
+    "documentation",
+    "isolation",
+    "debugging",
+  ],
+  intro:
+    "In unit tests we execute code in isolation, outside the application. The same approach could be applied to UI components, which proves to be a very convenient method during development and debugging, especially when dealing with external APIs or data sources difficult to control.",
+  isPublished: false,
+  isArchived: false,
+  cover: "/images/share/isolated-components-driven-development.png",
+};
