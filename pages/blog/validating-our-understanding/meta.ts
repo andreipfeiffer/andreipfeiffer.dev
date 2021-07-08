6ba3f7347275af0f2d2d77e818fe58a3cb45ec62
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-08 20:32:23 +0300

    Update article

--- pages/blog/validating-our-understanding/meta.ts
+++ pages/blog/validating-our-understanding/meta.ts
@@ -6,7 +6,7 @@ export const meta: Metadata = {
   tags: ["learning"],
   tags_extra: ["knowledge", "measure", "understanding", "analogy"],
   intro:
-    "There is no absolute measurement tool or technique to determine our level of understanding or a particular concept. However, there are some heuristics that we can use to evaluate ourselves.",
+    "There is no absolute measurement tool to determine our level of understanding of a particular concept. But there are specific heuristics that we can use to evaluate ourselves.",
   isPublished: false,
   isArchived: false,
   cover: "/images/share/validating-our-understanding.png",
