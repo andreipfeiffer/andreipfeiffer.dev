64678852e55b8c1957e7f7bed007d1a04e0fc8a5
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-07-12 18:45:30 +0300

    Update subscribe-thanks.page.tsx

--- pages/subscribe-thanks.page.tsx
+++ pages/subscribe-thanks.page.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
 
   return (
     <Layout>
-      <MetaTags title={`Thank you for subscribing at andreipfeiffer.dev`} />
+      <MetaTags title={`Thank you for subscribing to andreipfeiffer.dev`} />
 
       {breakpoint && <Spacer vertical="100" />}
 
@@ -25,15 +25,16 @@ export default function Contact() {
       <Spacer vertical="60" />
 
       <Grid.Full>
-        <Text as="p">
-          I will only send emails when new content is available, like blog posts
-          or workshops.
+        <Text as="p" size="p01">
+          Your subscription is confirmed!
         </Text>
 
+        <br />
+
         <Text as="p">
-          Your email is safe, as I will not share it with any 3rd party.
+          I really appreciate your interest in my content. <br />
+          <strong>Thank you dearly for your support.</strong>
         </Text>
-        <Text as="p">You can unsubscribe at anytime.</Text>
       </Grid.Full>
     </Layout>
   );
