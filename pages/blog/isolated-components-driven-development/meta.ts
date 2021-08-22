cd412fe07700e7dd34bbbbacbf3d74fcd4d409c2
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-08-22 20:17:18 +0300

    Update article

--- pages/blog/isolated-components-driven-development/meta.ts
+++ pages/blog/isolated-components-driven-development/meta.ts
@@ -2,8 +2,8 @@ import { Metadata } from "../../../lib/blog";
 
 export const meta: Metadata = {
   title: "Isolated Components Driven Development",
-  date: "2021-07-03",
-  tags: ["ui"],
+  date: "2021-08-30",
+  tags: ["ui", "react"],
   tags_extra: [
     "decoupling",
     "components",
@@ -12,7 +12,7 @@ export const meta: Metadata = {
     "debugging",
   ],
   intro:
-    "In unit tests, we execute code in isolation outside the application. The same approach could be applied to UI components, proving to be a very convenient method during development and debugging. As a bonus, this technique also provides component documentation, valuable to all team members working on the project.",
+    "In unit tests, we execute code in isolation, outside the application. The same approach could be applied to UI components, proving to be a very convenient method during development and debugging. As a bonus, this technique also provides documentation, valuable to all team members working on the project.",
   isPublished: false,
   isArchived: false,
   cover: "/images/share/isolated-components-driven-development.png",
