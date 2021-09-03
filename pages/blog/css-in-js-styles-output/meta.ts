45663ec2e725c99cf379d7d081b4caa2aff2a711
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-09-03 15:36:38 +0200

    Add new article

---
+++ pages/blog/css-in-js-styles-output/meta.ts
@@ -0,0 +1,21 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "CSS-in-JS styles output methods",
+  date: "2021-09-03",
+  tags: ["css", "javascript", "ui"],
+  tags_extra: [
+    "styling",
+    "css-in-js",
+    "performance",
+    "first paint",
+    "ssr",
+    "csr",
+    "critical css",
+  ],
+  intro:
+    "There are two mutually exclusive methods that CSS-in-JS libraries use to generate and ship styles to the browser. Both methods have benefits and downsides, so let’s analyze them in detail.",
+  isPublished: false,
+  isArchived: false,
+  // cover: "/images/share/tech-debt-tech-loan.png",
+};
