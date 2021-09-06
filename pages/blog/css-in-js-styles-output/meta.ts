dcca2586d3ea3a187db22b9f14c4d3689c05b4cb
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-09-06 11:24:26 +0200

    Publish article: CSS-in-JS styles output methods

--- pages/blog/css-in-js-styles-output/meta.ts
+++ pages/blog/css-in-js-styles-output/meta.ts
@@ -2,7 +2,7 @@ import { Metadata } from "../../../lib/blog";
 
 export const meta: Metadata = {
   title: "CSS-in-JS styles output methods",
-  date: "2021-09-03",
+  date: "2021-09-06",
   tags: ["css", "javascript", "ui"],
   tags_extra: [
     "styling",
@@ -14,8 +14,8 @@ export const meta: Metadata = {
     "critical css",
   ],
   intro:
-    "There are two mutually exclusive methods that CSS-in-JS libraries use to generate and ship styles to the browser. Both methods have benefits and downsides, so let’s analyze them in detail.",
-  isPublished: false,
+    "There are two mutually exclusive methods that CSS-in-JS libraries use to generate and ship styles to the browser: Runtime Stylesheets and Static CSS Extraction. Both methods have benefits and downsides, so let’s analyze them in detail.",
+  isPublished: true,
   isArchived: false,
-  // cover: "/images/share/tech-debt-tech-loan.png",
+  cover: "/images/share/css-in-js-styles-output.png",
 };
