f94dd1fe4450ca69e3af0afb62f35710f7b6edad
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-09-09 16:04:50 +0200

    Add scalable css series
    WIP

---
+++ pages/blog/scalable-css-evolution/meta.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "The evolution of scalable CSS",
+  date: "2021-12-28",
+  tags: ["css", "ui"],
+  tags_extra: ["scalability", "maintainability"],
+  intro: "We came a long way...",
+  isPublished: false,
+  isArchived: false,
+  // cover: "/images/share/tech-debt-tech-loan.png",
+};
---
+++ pages/blog/scalable-css-evolution/part5-encapsulation/meta.ts
@@ -0,0 +1,13 @@
+import { Metadata } from "../../../../lib/blog";
+
+export const meta: Metadata = {
+  title: "The evolution of scalable CSS",
+  subtitle: "Part 4: Methodologies and frameworks",
+  date: "2021-12-28",
+  tags: ["css", "ui"],
+  tags_extra: ["scalability", "maintainability"],
+  intro: "We came a long way...",
+  isPublished: false,
+  isArchived: false,
+  // cover: "/images/share/tech-debt-tech-loan.png",
+};
