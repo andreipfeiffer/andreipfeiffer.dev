17426232c7853ecd3b9e09769ac4a804b184b761
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-09-20 17:27:49 +0200

    Publish article: The anatomy of React component files

--- pages/blog/react-components-anatomy/meta.ts
+++ pages/blog/react-components-anatomy/meta.ts
@@ -2,11 +2,11 @@ import { Metadata } from "../../../lib/blog";
 
 export const meta: Metadata = {
   title: "The anatomy of React component files",
-  date: "2021-09-18",
+  date: "2021-09-21",
   tags: ["react", "javascript", "typescript"],
   tags_extra: ["structure", "functions", "components", "imports"],
   intro:
     "This article describes in detail my personal and current approach to defining React component files and the rationale for each decision. It covers function components syntax with TypeScript.",
-  visibility: "unlisted",
-  cover: "/images/share/react-components-anatomy.png.png",
+  visibility: "public",
+  cover: "/images/share/react-components-anatomy.png",
 };
