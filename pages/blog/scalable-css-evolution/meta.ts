9179a562dca01044f3cc062be1d8b36f2a72483a
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-12-02 22:43:37 +0200

    Update css series images and caption/alt texts

--- pages/blog/scalable-css-evolution/meta.ts
+++ pages/blog/scalable-css-evolution/meta.ts
@@ -1,6 +1,6 @@
 import { Metadata } from "../../../lib/blog";
 
-import cover from "./scalable-css-evolution-dark.png";
+import cover from "./scalable-css-evolution-light.gif";
 
 export const meta: Metadata = {
   title: "The evolution of scalable CSS",
@@ -10,6 +10,5 @@ export const meta: Metadata = {
   intro:
     "The evolution of scalable CSS is a multi-part chronicle intended to document the progress of tools and practices that enable us to write maintainable CSS at scale. It covers CSS scalability issues, CSS processors, essential good practices, methodologies, semantic and atomic CSS, styles encapsulation, CSS-in-JS, and type-safe CSS with Typescript.",
   visibility: "private",
-  // cover: "/scalable-css-evolution.png",
   cover: cover.src,
 };
