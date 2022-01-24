301d20ab3fbadcab8be9439a4dfcfa72b262b390
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-01-24 13:32:55 +0200

    Publish Scalable CSS, part 0

--- pages/blog/2022/scalable-css-evolution/meta.ts
+++ pages/blog/2022/scalable-css-evolution/meta.ts
@@ -4,12 +4,12 @@ import cover from "./scalable-css-evolution-cover.png";
 
 export const meta: Metadata = {
   title: "The evolution of scalable CSS",
-  date: "2021-12-28",
+  date: "2022-01-25",
   tags: ["css", "javascript", "typescript"],
   tags_extra: ["scalability", "maintainability", "styling"],
   intro:
     "The evolution of scalable CSS is a multi-part chronicle, intended to document the progress of tools and practices that enable us to write maintainable CSS at scale. It covers CSS scalability issues, CSS processors, essential good practices, methodologies, semantic and atomic CSS, styles encapsulation, CSS-in-JS, and type-safe CSS with Typescript.",
-  visibility: "private",
+  visibility: "public",
   cover: cover.src,
   cover_width: 2000,
   cover_height: 1000,
