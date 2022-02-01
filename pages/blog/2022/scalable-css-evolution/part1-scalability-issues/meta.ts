ce37b378db8a6778fb479a6f8f967fb970e827c4
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-02-01 16:36:32 +0200

    Publish Scalable CSS, part 1

--- pages/blog/2022/scalable-css-evolution/part1-scalability-issues/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part1-scalability-issues/meta.ts
@@ -6,11 +6,11 @@ import cover from "./scalability-issues-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 1: CSS scalability issues",
-  date: "2021-12-28",
+  date: "2022-02-02",
   tags: ["css"],
   tags_extra: ["scalability", "maintainability", "styling", "limitations"],
   intro:
-    "Before discussing the solutions to CSS scalability issues, we must understand what these issues are in the first place. Thus, let's take a look at some of the most concerning problems when talking about long-term CSS maintainability.",
-  visibility: "private",
+    "Before discussing the solutions to CSS scalability issues, we must understand what these concerns are in the first place. Thus, let's take a look at some of the most concerning problems when talking about long-term CSS maintainability.",
+  visibility: "public",
   cover: cover.src,
 };
