114996e021269e2939dbbd56325b4c93524d0ac3
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-02-02 18:25:55 +0200

    Finalize css series, part 6

--- pages/blog/2022/scalable-css-evolution/part6-atomic-css/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part6-atomic-css/meta.ts
@@ -7,16 +7,19 @@ export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 6: Atomic CSS",
   date: "2021-12-28",
-  tags: ["css", "ui"],
+  tags: ["css"],
   tags_extra: [
+    "reusability",
+    "semantics",
     "scalability",
     "maintainability",
     "styling",
     "utility-first",
+    "atomic css",
     "functional css",
   ],
   intro:
-    "Atomic CSS contradicts and breaks all the principles of Semantic CSS. However, this mindset shift gave birth to a whole new set of frameworks, based solely on single-purpose utility classes.",
+    "Atomic CSS contradicts and breaks all the principles of Semantic CSS. This mindset gave birth to a new set of frameworks based solely on single-purpose utility classes.",
   visibility: "draft",
   cover: cover.src,
 };
