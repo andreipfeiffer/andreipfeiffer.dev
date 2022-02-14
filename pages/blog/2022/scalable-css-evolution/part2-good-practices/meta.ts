87f78a9d6941f95fcbf8d72d02d581ff6f87fe48
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-02-14 17:22:47 +0200

    Update Scalable CSS, part 2

--- pages/blog/2022/scalable-css-evolution/part2-good-practices/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part2-good-practices/meta.ts
@@ -6,11 +6,11 @@ import cover from "./good-practices-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 2: Good practices",
-  date: "2022-02-11",
+  date: "2022-02-14",
   tags: ["css"],
   tags_extra: ["scalability", "maintainability", "styling", "specificity"],
   intro:
-    "Avoiding naming collisions and specificity wars is not a trivial task. Therefore, let's explore in detail several fundamental practices that reached consensus within the development community when talking about scalable and maintainable CSS.",
-  visibility: "private",
+    "Avoiding naming collisions or specificity wars is not a trivial task. Therefore, let's explore in detail several fundamental practices that reached consensus within the development community when talking about scalable and maintainable CSS.",
+  visibility: "unlisted",
   cover: cover.src,
 };
