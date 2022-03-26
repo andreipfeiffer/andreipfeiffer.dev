1dc3faf945fdbdefe48ffc6ab8be57b595d825c3
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-03-26 15:44:18 +0200

    Publish Scalable CSS, part 4

--- pages/blog/2022/scalable-css-evolution/part4-methologies-and-semantics/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part4-methologies-and-semantics/meta.ts
@@ -6,8 +6,8 @@ import cover from "./methodologies-semantics-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 4: Methodologies and Semantics",
-  date: "2021-12-28",
-  tags: ["css", "ui"],
+  date: "2022-03-26",
+  tags: ["css"],
   tags_extra: [
     "scalability",
     "maintainability",
@@ -18,9 +18,10 @@ export const meta: Metadata = {
     "css architecture",
     "semantics",
     "components",
+    "user interface",
   ],
   intro:
     "Most CSS methodologies and architectures are built on top of many good practices that we've previously covered, providing a structured and cohesive set of principles and rules. In this chapter we'll focus on Semantic CSS, encouraged by the HTML5 specification, which most CSS methodologies and frameworks embrace.",
-  visibility: "private",
+  visibility: "unlisted",
   cover: cover.src,
 };
