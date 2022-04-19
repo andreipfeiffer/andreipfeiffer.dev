305b90b99cd19b15542891c81a8c293c059913c1
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-04-19 14:48:39 +0300

    Publish Scalable CSS, part 5

--- pages/blog/2022/scalable-css-evolution/part5-styles-encapsulation/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part5-styles-encapsulation/meta.ts
@@ -6,7 +6,7 @@ import cover from "./styles-encapsulation-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 5: Styles Encapsulation",
-  date: "2021-12-28",
+  date: "2021-04-19",
   tags: ["css"],
   tags_extra: [
     "scalability",
@@ -14,9 +14,10 @@ export const meta: Metadata = {
     "styling",
     "css modules",
     "scoped css",
+    "shadow dom"
   ],
   intro:
-    "CSS Modules and styles encapsulation in general elegantly solve all the CSS naming collisions problems, while keeping specificity low at the same time. These techniques profoundly changed the way we author semantic CSS nowadays and became an industry standard, adopted by all component-based JavaScript frameworks.",
-  visibility: "private",
+    "Styles encapsulation and CSS Modules elegantly solve all the CSS naming collisions problems, while keeping specificity low at the same time. These techniques profoundly changed the way we author semantic CSS nowadays and became an industry standard, adopted by all component-based JavaScript frameworks.",
+  visibility: "unlisted",
   cover: cover.src,
 };
