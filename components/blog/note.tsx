9522668f591ad46e3f4845c16abff4afa5242d9d
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-04-29 15:38:51 +0300

    Add optional uppercase prop to Note

--- components/blog/note.tsx
+++ components/blog/note.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 
 import styles from "./note.module.scss";
@@ -5,12 +6,17 @@ import styles from "./note.module.scss";
 type Props = {
   children: React.ReactNode;
   title?: string;
+  uppercase?: boolean;
 };
 
-export function Note({ children, title = "Note" }: Props) {
+export function Note({ children, title = "Note", uppercase = true }: Props) {
   return (
     <div className={styles.note}>
-      <strong className={styles.title}>{title}</strong>
+      <strong
+        className={classNames(styles.title, { [styles.uppercase]: uppercase })}
+      >
+        {title}
+      </strong>
       {children}
     </div>
   );
