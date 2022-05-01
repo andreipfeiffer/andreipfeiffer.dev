c7230c6848a10b72ffa09e401823cc197b9b2f56
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-05-01 17:48:43 +0300

    Add new article: CSS-in-JS misconceptions

---
+++ pages/blog/2022/css-in-js-misconceptions/meta.ts
@@ -0,0 +1,14 @@
+import { Metadata } from "../../../../lib/blog";
+
+import cover from "./css-in-js-misconceptions-cover.png";
+
+export const meta: Metadata = {
+  title: "CSS-in-JS misconceptions",
+  date: "2022-05-28",
+  tags: ["css", "javascript"],
+  tags_extra: [],
+  intro:
+    "Despite its versatility and potential, CSS-in-JS received a significant kickback from the community. We'll analyze the most recurring misconceptions, shedding some light regarding what CSS-in-JS is, but more importantly, what CSS-in-JS is not.",
+  visibility: "unlisted",
+  cover: cover.src,
+};
