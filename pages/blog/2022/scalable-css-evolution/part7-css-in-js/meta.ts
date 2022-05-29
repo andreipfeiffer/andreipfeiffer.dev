b643b63d0fdd4977c30f54cf7b9f2724a2d90577
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-05-29 17:02:45 +0300

    Publish Scalable CSS, part 7

--- pages/blog/2022/scalable-css-evolution/part7-css-in-js/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part7-css-in-js/meta.ts
@@ -6,11 +6,11 @@ import cover from "./css-in-js-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Part 7: CSS-in-JS",
-  date: "2021-12-28",
+  date: "2022-05-30",
   tags: ["css", "javascript"],
   tags_extra: ["scalability", "maintainability", "styling"],
   intro:
     "Moving style definitions to JavaScript files is a novel approach to CSS, resulting in countless benefits, such as explicit dependencies, shared variables, not to mention all the features of styles encapsulation, CSS preprocessors, Atomic CSS, and so much more.",
-  visibility: "private",
+  visibility: "unlisted",
   cover: cover.src,
 };
