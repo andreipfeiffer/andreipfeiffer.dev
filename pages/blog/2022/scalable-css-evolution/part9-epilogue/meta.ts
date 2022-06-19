a74d671b8e9dcb40fafd3e5544d29da6ca27cd5a
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-06-19 14:36:04 +0300

    Update css series, epilogue

--- pages/blog/2022/scalable-css-evolution/part9-epilogue/meta.ts
+++ pages/blog/2022/scalable-css-evolution/part9-epilogue/meta.ts
@@ -6,10 +6,11 @@ import cover from "./scalable-css-epilogue-cover.png";
 export const meta: Metadata = {
   ...common_meta,
   subtitle: "Epilogue",
-  date: "2021-12-28",
+  date: "2022-06-20",
   tags: ["css"],
-  tags_extra: ["scalability", "maintainability", "styling"],
-  intro: "???",
-  visibility: "draft",
+  tags_extra: ["scalability", "maintainability", "styling", "paradigms"],
+  intro:
+    "Before we conclude this chronicle, let's take a step back, reflect on the journey we had, and synthesize our options to address scalable CSS.",
+  visibility: "unlisted",
   cover: cover.src,
 };
