c98e959fa3597073fda223a3534aeba499ff4482
Andrei Pfeiffer <andrei@upsidedown.ro>
2022-07-08 12:17:49 +0300

    Publish article

--- pages/blog/2022/fixing-long-npm-installs/meta.ts
+++ pages/blog/2022/fixing-long-npm-installs/meta.ts
@@ -10,6 +10,6 @@ export const meta: Metadata = {
   tags_extra: ["npm", "dependencies", "slow", "optimization"],
   intro:
     "In this case study, we'll analyze how to detect npm packages with long instalation times and how to potentially optimize them. We have faced this problem during builds on the CI server.",
-  visibility: "draft",
+  visibility: "public",
   cover: cover.src,
 };
