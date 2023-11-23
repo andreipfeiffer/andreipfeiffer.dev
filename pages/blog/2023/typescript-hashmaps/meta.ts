5e0827b2173674b14e3d6d40ef57367f7a1e5ca7
Andrei Pfeiffer <andrei@upsidedown.ro>
2023-11-23 19:37:52 +0200

    New article: TypeScript Hash Maps

---
+++ pages/blog/2023/typescript-hashmaps/meta.ts
@@ -0,0 +1,14 @@
+import { Metadata } from "../../../../lib/blog";
+
+import cover from "./typescript-hashmaps-cover.png";
+
+export const meta: Metadata = {
+  title: "Hash Maps in TypeScript",
+  date: "2023-11-23",
+  tags: ["typescript"],
+  tags_extra: [],
+  intro:
+    "In this post, we'll explore two different typical methods to implement Hash Maps with optional items in TypeScript and analyze the member access type-safety.",
+  visibility: "private",
+  cover: cover.src,
+};
