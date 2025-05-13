ea9c7533b1f009c009db054ee63ac56972e4d651
Andrei Pfeiffer <andrei@upsidedown.ro>
2025-05-13 19:35:42 +0300

    Update meta.ts

--- pages/blog/2025/the-code-etymologist/meta.ts
+++ pages/blog/2025/the-code-etymologist/meta.ts
@@ -1,5 +1,6 @@
 import { Metadata } from "../../../../lib/blog";
 
+
 // import cover from "./excuses-for-not-writing-tests-cover.png";
 
 export const meta: Metadata = {
@@ -9,6 +10,6 @@ export const meta: Metadata = {
   tags_extra: [],
   intro:
     "The code etymologist is a multi-part series that covers typical software knowledge that deserves to be documented and shared within development teams, such as: project setup, coding guidelines, development workflows, features, UI components library, data structures, and technical decisions.",
-  visibility: "draft",
+  visibility: "private",
   cover: "",
 };
