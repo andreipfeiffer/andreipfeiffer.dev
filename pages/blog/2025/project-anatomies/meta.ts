e1254e644a10fa317bf94eaaf88ef8a11391107d
Andrei Pfeiffer <andrei@upsidedown.ro>
2026-01-11 10:01:38 +0200

    Publish Project Anatomies

--- pages/blog/2025/project-anatomies/meta.ts
+++ pages/blog/2025/project-anatomies/meta.ts
@@ -1,13 +1,16 @@
 import { Metadata } from "../../../../lib/blog";
 
-// import cover from "./the-code-etymologist-cover.png";
+
+
+import cover from "./project-anatomies-cover.png";
 
 export const meta: Metadata = {
   title: "The Complete Map of Project Anatomies",
   date: "2025-06-28",
   tags: ["entropy"],
-  tags_extra: [],
-  intro: "",
-  visibility: "unlisted",
-  // cover: cover.src,
+  tags_extra: ["struture", "files", "folders"],
+  intro:
+    "In this blog post we'll analyse hospitals distribution at different scales and see that software projects follow a similar evolution regarding their files and folders anatomy.",
+  visibility: "public",
+  cover: cover.src,
 };
