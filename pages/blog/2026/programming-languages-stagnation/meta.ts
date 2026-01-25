01acd1b1165abec7016975c373b39800f0682e6b
Andrei Pfeiffer <andrei@upsidedown.ro>
2026-01-25 14:51:33 +0200

    Add new article: Programming languages stagnation

---
+++ pages/blog/2026/programming-languages-stagnation/meta.ts
@@ -0,0 +1,14 @@
+import { Metadata } from "../../../../lib/blog";
+
+import cover from "./programming-languages-stagnation-cover.png";
+
+export const meta: Metadata = {
+  title: "The stagnation of programming languages?",
+  date: "2026-01-25",
+  tags: ["learning"],
+  tags_extra: ["ai"],
+  intro:
+    "Will LLMs learn new language syntax and new standard features, considering that most developers don't manually write code anymore and accept LLM output with the existing training data?",
+  visibility: "unlisted",
+  cover: cover.src,
+};
