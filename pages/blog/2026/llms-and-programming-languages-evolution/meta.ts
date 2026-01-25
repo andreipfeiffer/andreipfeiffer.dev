23f3662d1fc4386292cc39836c0638f1d8eeb1c5
Andrei Pfeiffer <andrei@upsidedown.ro>
2026-01-25 15:48:09 +0200

    Publish LLMs and programming languages evolution

--- pages/blog/2026/llms-and-programming-languages-evolution/meta.ts
+++ pages/blog/2026/llms-and-programming-languages-evolution/meta.ts
@@ -1,6 +1,9 @@
 import { Metadata } from "../../../../lib/blog";
 
+
+
 import cover from "./llms-and-programming-languages-evolution-cover.png";
+
 
 export const meta: Metadata = {
   title: "Will LLMs evolve with programming languages?",
@@ -9,6 +12,6 @@ export const meta: Metadata = {
   tags_extra: ["ai"],
   intro:
     "Will LLMs learn new language syntax and new standard features, considering that most developers don't manually write code anymore and accept LLM output with the existing training data?",
-  visibility: "unlisted",
+  visibility: "public",
   cover: cover.src,
 };
